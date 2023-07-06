7d362fef90a04d01ade722fa224d3dd7c74a68b4
debyat <johnstiger885@gmail.com>
2023-07-06 11:36:56 +0800

    Initial Commit

--- carousel.js
+++ carousel.js
@@ -1,8 +1,10 @@
-let displayGoogle = true, displayYelp = true, displayFacebook = true;
+let displayGoogle = true, displayYelp = true, displayFacebook = true, googleRatings = 0.0;
 
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const tabBtns = document.querySelectorAll(".btns button");
+
+let type = [];
 
 let displayTabs = [
     {
@@ -58,6 +60,58 @@ let all = [
     }
 ];
 
+async function getGoogleReviews() {
+    await fetch('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJLV2HitsliYgRDP-cgzHt1CI&fields=name,rating,reviews&key=AIzaSyAHRcJ3LBuRPcnoOmx-VfjemwiBPs3UE7M', {
+        method: 'GET',
+        })
+        .then(response => response.json())
+        .then(response => {
+            googleRatings = response.result.rating
+            all = response.result.reviews.map(res => {
+                return {
+                    'name': res.author_name,
+                    'profile_pic': res.profile_photo_url,
+                    'rating': res.rating,
+                    'comment': res.text,
+                    'date': res.time,
+                    'profile_link': res.author_url,
+                    'type': 'google'
+                }
+            })
+        })
+        .catch(err => console.error(err));
+}  
+
+async function getYelpReviews() {
+    await fetch('https://api.yelp.com/v3/businesses/xaDEiJ0N0SYOv-u0JXBSNQ/reviews?limit=20&sort_by=yelp_sort', {
+        method: 'GET',
+        headers: {
+            "accept": 'application/json',
+            "Authorization": 'Bearer yugTEZuzyqh5VDh7lDWNtqy9mLmi3-W5lgXTjf45a_8_6cswDP0UOoKInNaRM0ckn8rFEhtnME0MvRQ-gParjtflvsovNDswGuUnapmL8h1RHU9evUIZDNYG-xamZHYx',
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Credentials": "true",
+        }
+        })
+        .then(response => response.json())
+        .then(response => {
+            response.reviews.forEach(res => {
+                let ap = {
+                            'name': res.name,
+                            'profile_pic': res.user.image_url,
+                            'rating': res.rating,
+                            'comment': res.text,
+                            'date': res.time_created,
+                            'profile_link': res.profile_url,
+                            'type': 'google'
+                        }
+                all.push(ap)
+            })
+        })
+        .catch(err => console.error(err));
+}  
+
 // Display tabs
 displayTabs.forEach(tab => {
     if (tab.display) {
@@ -80,12 +134,15 @@ function displayCards(display) {
     all.forEach(content => {
         if (display == 'all') {
             displayTabs.forEach(tab => {
+                console.log(content)
                 if (tab.label == content.type && tab.display) {
                     visibles++;
                     carousel.innerHTML += `<li class="card">
-                                            <div class="img"><img src="${content.image}" draggable="false"></div>
-                                            <h2>${content.title}</h2>
-                                            <span>${content.span}</span>
+                                            <div class="img"><img src="${content.profile_pic}" draggable="false"></div>
+                                            <h2>${content.name}</h2>
+                                            <span>${content.rating}</span>
+                                            <span>${content.date}</span>
+                                            <span>${content.comment}</span>
                                             <span>${content.type}</span>
                                             </li>`;
                 }
@@ -140,74 +197,87 @@ function duplicate(firstCardWidth, carouselChildrens, visibles) {
     }
 }
 
-// Calling the method
-displayCards('all')
 
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+async function main() {
+    await getGoogleReviews(); 
+    await getYelpReviews();
 
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
+    displayCards('all')
 
-// Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+    const arrowBtns = document.querySelectorAll(".wrapper i");
+    const carouselChildrens = [...carousel.children];
+    let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+    // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+
+    // Add event listeners for the arrow buttons to scroll the carousel left and right
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+        });
     });
-});
 
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
-
-const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
+    const dragStart = (e) => {
+        isDragging = true;
+        carousel.classList.add("dragging");
+        // Records the initial cursor and scroll position of the carousel
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
     }
 
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
+    const dragging = (e) => {
+        if(!isDragging) return; // if isDragging is false return from here
+        // Updates the scroll position of the carousel based on the cursor movement
+        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    }
 
-const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
-autoPlay();
+    const dragStop = () => {
+        isDragging = false;
+        carousel.classList.remove("dragging");
+    }
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("scroll", infiniteScroll);
-wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-wrapper.addEventListener("mouseleave", autoPlay);
+    const infiniteScroll = () => {
+        // If the carousel is at the beginning, scroll to the end
+        if(carousel.scrollLeft === 0) {
+            carousel.classList.add("no-transition");
+            carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+            carousel.classList.remove("no-transition");
+        }
+        // If the carousel is at the end, scroll to the beginning
+        else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+            carousel.classList.add("no-transition");
+            carousel.scrollLeft = carousel.offsetWidth;
+            carousel.classList.remove("no-transition");
+        }
+
+        // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+        clearTimeout(timeoutId);
+        if(!wrapper.matches(":hover")) autoPlay();
+    }
+
+    const autoPlay = () => {
+        if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+        // Autoplay the carousel after every 2500 ms
+        timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+    }
+    autoPlay();
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    carousel.addEventListener("scroll", infiniteScroll);
+    wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+    wrapper.addEventListener("mouseleave", autoPlay);
+}
+    
+
+// Calling the method
+main()
+
+
+
+
